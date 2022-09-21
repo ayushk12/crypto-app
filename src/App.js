6ec549c53a49d4c908bd0744343d05561e2229b2
ayushk12 <kayush242@gmail.com>
2022-09-21 10:41:45 +0530

    setup ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios"
 
 function App() {
+  const [coins,setCoins]=useState("");
+  const url='';
+  useEffect(()=>{
+
+    axios.get(url).then((res)=>{
+      setCoins(res.data)
+      console.log(res.data[0])
+
+    }).catch((error)=>{
+      console.log(error)
+    })
+    
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <p>Crypto App</p>
     </div>
   );
 }
